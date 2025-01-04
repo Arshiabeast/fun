8e1ba8be0d0079fd3fd1c2c614ba28a0b6048d40
TOT <infodigital65@gmail.com>
2025-01-04 13:23:34 -0800

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import os
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
 from telegram.ext import CallbackContext
@@ -13,7 +14,12 @@ async def start(update: Update, context: CallbackContext):
     await update.message.reply_text("Hello! I will welcome new users in the group.")
 
 def main():
-    application = Application.builder().token('8137611209:AAE-KVSrchV9hX5w0FVr70FZNbWiPsuxma0').build()
+    # بارگذاری توکن از متغیر محیطی
+    token = os.getenv('TELEGRAM_TOKEN')
+    if not token:
+        raise ValueError("No token found! Please set the TELEGRAM_TOKEN environment variable.")
+    
+    application = Application.builder().token(token).build()
 
     # تنظیم handler برای خوش‌آمدگویی به کاربران جدید
     application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome))
